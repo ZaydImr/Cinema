debdd68962e6de4b49f6873fbedc1d48ce9022f7
Zayd ElImrani <elimrani.z@gmail.com>
2021-12-23 11:50:01 +0100

    Home page ..

--- src/main/resources/static/js/controllers/admin/dashboard.js
+++ src/main/resources/static/js/controllers/admin/dashboard.js
@@ -3,6 +3,7 @@ myApp.controller("dashboardController", function ($scope, $http) {
   $scope.totalClients = 0;
   $scope.totalFilms = 0;
   $scope.subscribers = 0;
+  $scope.monthVisitors = [];
 
   $scope.getFilmsCount = function () {
     $http.get("/api/film/count").then(
@@ -39,4 +40,36 @@ myApp.controller("dashboardController", function ($scope, $http) {
       }
     );
   };
+
+  $scope.getVisitorsPerDay = function () {
+    $http.get("/api/visitors/today").then(
+      function successCallback(response) {
+        $scope.todayVisitors = response.data;
+      },
+      function errorCallback(response) {
+        console.log("Error....");
+        console.log(response);
+      }
+    );
+  };
+
+  $scope.getVisitors = function () {
+    $http.get("/api/visitors/month").then(
+      function successCallback(response) {
+        $scope.monthVisitors = response.data;
+        console.log($scope.monthVisitors);
+      },
+      function errorCallback(response) {
+        console.log("Error....");
+        console.log(response);
+      }
+    );
+  };
+
+  $scope.getFilmsCount()
+  $scope.getSubscribersCount()
+  $scope.getUsersCount()
+  $scope.getVisitorsPerDay()
+  $scope.getVisitors()
+
 });
