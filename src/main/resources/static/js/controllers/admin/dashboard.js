a552867c2cbf74860945ddb99fe8f3e12ee7777e
Zayd ElImrani <elimrani.z@gmail.com>
2021-12-23 12:45:05 +0100

    Finishing dashboard

--- src/main/resources/static/js/controllers/admin/dashboard.js
+++ src/main/resources/static/js/controllers/admin/dashboard.js
@@ -1,4 +1,18 @@
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
 myApp.controller("dashboardController", function ($scope, $http) {
+
+  let today = new Date();
+  let month = [];
+
+  for (let i = -29; i <= 0; i++) {
+    month.push(today.addDays(i).getDate())
+  }
+
   $scope.todayVisitors = 0;
   $scope.totalClients = 0;
   $scope.totalFilms = 0;
@@ -56,8 +70,62 @@ myApp.controller("dashboardController", function ($scope, $http) {
   $scope.getVisitors = function () {
     $http.get("/api/visitors/month").then(
       function successCallback(response) {
+
         $scope.monthVisitors = response.data;
-        console.log($scope.monthVisitors);
+
+        const lineConfig = {
+          type: 'line',
+          data: {
+            labels: month,
+            datasets: [
+              {
+                label: 'Visitors per day',
+                backgroundColor: '#0694a2',
+                borderColor: '#0694a2',
+                data: response.data,
+                fill: false,
+              }
+            ],
+          },
+          options: {
+            responsive: true,
+            /**
+             * Default legends are ugly and impossible to style.
+             * See examples in charts.html to add your own legends
+             *  */
+            legend: {
+              display: false,
+            },
+            tooltips: {
+              mode: 'index',
+              intersect: false,
+            },
+            hover: {
+              mode: 'nearest',
+              intersect: true,
+            },
+            scales: {
+              x: {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Month',
+                },
+              },
+              y: {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Value',
+                },
+              },
+            },
+          },
+        }
+
+        const lineCtx = document.getElementById('line');
+        window.myLine = new Chart(lineCtx, lineConfig);
+
       },
       function errorCallback(response) {
         console.log("Error....");
